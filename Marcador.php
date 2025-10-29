2b83efacd19670b92e41189ccd523a95156b0a35
new-CarlosEst <c.estebandiez@gmail.com>
2025-10-29 17:40:00 +0100

    Clase Marcador y clase contendora Marcadores
    Terminada la clase marcador con todos sus atributos, el constructor, getters y un toString
    
    Ademas creadad la clase Marcadores que tendra una lista con todos los datos de la tabla topten en forma de objeto Marcador

--- Marcador.php
+++ Marcador.php
@@ -1,5 +1,38 @@
 <?php
+    /**
+     * Clase marcador con los atributos de la tabla topten
+     * 
+     * @author Carlos Esteban Diez 2 Daw
+     * 
+     * @param $posicion Posicion del jugador segun su puntuacion
+     * @param $puntuacion puntuacion del jugador
+     * @param $nick Nombre del jugador
+     */
     class Marcador {
-        
+        private $posicion;
+        private $puntuacion;
+        private $nick;
+
+        public function __construct(?int $posicion, ?int $puntuacion, ?string $nick){
+            $this->posicion = $posicion;
+            $this->puntuacion = $puntuacion;
+            $this->nick = $nick;
+        }
+
+        public function getPosicion(){
+            return $this->posicion;
+        }
+
+        public function getPuntuacion(){
+            return $this->puntuacion;
+        }
+
+        public function getNick(){
+            return $this->nick;
+        }
+
+        public function __toString () : string {
+            return $this->posicion . ";" . $this->puntuacion . ";" . $this->nick;
+        }
     }
 ?>
---
+++ Marcadores.php
@@ -0,0 +1,16 @@
+<?php 
+    /**
+     * Clase contendora de la clase Marcador
+     * Esta clase contendra una lista con los datos de la tabla top ten en forma de objeto Marcador
+     * 
+     * @author Carlos Esteban Diez
+     * 
+     * @param $listaMarcador Array con varios objetos Marcador
+     * 
+     * @param $Conexion Conexion a la base de datos donde sacare los datos
+     */
+    class Marcadores {
+        private $listaMarcador;
+        private $conexion;
+    }
+?>
