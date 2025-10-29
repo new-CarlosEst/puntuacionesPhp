aa5b5edc0c139a78e9978f2b8b57865858fd0170
new-CarlosEst <c.estebandiez@gmail.com>
2025-10-29 17:13:45 +0100

    Conexion al db y clase marcador
    creada la conexion a la base de datos, esta me carga un sql donde me crea una db si no esta creada ya. Despues crea una tabla si no existe en esa base de datos y me hace los inserts
    
    Por ultimo me hago la conexion a esa db y me he creado la clase marcador

--- ConexionBBDD.php
+++ ConexionBBDD.php
@@ -1,5 +1,89 @@
 <?php 
+    //Cargo el archivo autoload.php para cargar las liberias
+    require_once __DIR__ . "/vendor/autoload.php";
+
+    //Cargo el Dotenv
+    //!siempre se tiene que cargar despues de el require/include
+    use Dotenv\Dotenv;
+
+    //Cargo el .env (en este caso pongo solo __DIR__ ya que el .env esta en la misma carpeta que el .php)
+    $dotenv = Dotenv::createImmutable(__DIR__); 
+    $dotenv->load();
+    /**
+     * Clase para conectase a la base de datos
+     * 
+     * @author Carlos Esteban Diez 2 Daw
+     * @param $user Usuario para conectarse a la bbdd
+     * @param $password Contraseña para conectarse
+     * @param $db Nombre de la base de datos
+     * @param $service Servicio de la base de datos o el puerto al que se conecta
+     * @param $conexion Variable donde se guarda la conexion pdo
+     */
+
+
     class Conexion {
-        //Si lo haces privado y estatico solo tienes una conexion
+        //Si lo haces privado y estatico solo tiene
+        private $user;
+        private $password;
+        private $db;
+        private $service;
+        private $conexion;
+
+        public function __construct() {
+            //Me saco el usuario, password, db y servicio del .env
+            $this->user = $_ENV["USER"];
+            $this->password = $_ENV["PASS"];
+            $this->db = $_ENV["DB"];
+            $this->service = $_ENV["SERVICE"];
+
+            //Me cargo el el sql
+            $this->cargarSql();
+
+            //Me hago la conexion a la base de datos correcta
+            $this->conectarse();
+        }
+
+        /** 
+         * Esta funcion me conectara a mi servidor mysql pero no a la base de datos 
+         * Despues cojera y me ejecutar el sql creando la base de datos
+         */
+        private function cargarSql (){
+            try{
+                //me hago una conexion para cargar el sql temporal
+                $conexion = new PDO("mysql:host=". $this->service, $this->user, $this->password);
+                //Pongo los errores y excepciones
+                $conexion-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                //Saco la ruta al .sql
+                $archivoSQL = __DIR__ . '/resources/marcador_v2.sql';
+                
+                //Compruebo que exista el .sql
+                if (!file_exists($archivoSQL)){
+                    echo "Archivo .sql no encontrado";
+                }
+                else {
+                    //Saco los datos del .sql
+                    $contenidoSQL = file_get_contents($archivoSQL);
+                    //Ejecuto lo que hay en el sql
+                    $conexion->exec($contenidoSQL);
+                }
+            } catch (PDOException $e){
+                echo "Error la conectarser a la base de datos <br/>" . $e;
+            }
+        }
+
+        public function conectarse(){
+            try {
+                //Me hago la conexion y pongo los errores
+                $conexion = new PDO ("mysql:host=". $this->service . ";dbname=". $this->db, $this->user, $this->password);
+                $conexion-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                //devuelvo esta conexion 
+                return $conexion;
+                
+            } catch (PDOException $e){
+                echo "Error la conectarser a la base de datos <br/>" . $e;
+            }
+        }
     }
 ?>
---
+++ resources/marcador_v2.sql
@@ -0,0 +1,25 @@
+-- Cambio un poco el script para que solo me cree la base de datos y la tabla si no existe asi mantengo los datos anteriores 
+-- y no se me ponen estos cada vez que inicializo la conexion
+CREATE DATABASE IF NOT EXISTS marcador;
+USE marcador;
+-- Tablas
+CREATE TABLE IF NOT EXISTS topten (
+pos int primary key,				-- Posicion en el marcador
+nick varchar(3),					-- Nick del jugador ( 3 letras )						
+score  int							-- Puntuación >=0
+);
+
+-- Datos 
+-- Cambio un poco el sql y me hago un insert ingnore para que si ya esta esa posicion (Esa primary key) no me hago el insert
+INSERT IGNORE INTO topten values (1,'ZOO', 9991);
+INSERT IGNORE INTO topten values (2,'JAR', 8523);
+INSERT IGNORE INTO topten values (3,'ZIP', 3389);
+INSERT IGNORE INTO topten values (4,'TAR', 1564);
+INSERT IGNORE INTO topten values (5,'BZ7', 792);
+INSERT IGNORE INTO topten values (6,'RAR', 666);
+INSERT IGNORE INTO topten values (7,'---', 0);
+INSERT IGNORE INTO topten values (8,'---', 0);
+INSERT IGNORE INTO topten values (9,'---', 0);
+INSERT IGNORE INTO topten values (10,'---', 0);
+
+-- select * from topten;
